d3d07930b7b3bb521b0c8eaf534ef224bb08da5d
David Rojas <david_rojas82142@elpoli.edu.co>
2020-05-20 12:28:12 -0500

    start

---
+++ src/test/java/com/example/Meven/MevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Meven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
